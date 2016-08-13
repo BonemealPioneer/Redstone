d93732b022274298fa20db66b45767a3916983b2
BonemealPioneer <thebonemealpioneer1@gmail.com>
2016-08-13 15:46:58 -0400

    Added base for world management and worlds

---
+++ redstone/world/IWorldManager.py
@@ -0,0 +1,32 @@
+from zope.interface import Interface
+from zope.interface import implements
+
+from redstone.world.World import World
+
+class IWorldManager(object):
+    """
+    A class for managing multiple worlds
+    """
+
+    implements(Interface)
+
+    def __init__(self):
+        self.loaded_worlds = {}
+
+    def get_world_loaded(self, world_name):
+        if world_name in self.loaded_worlds.keys():
+            return True
+
+        return False
+
+    def load_world(self):
+        """
+        Stores as {World Name}: World Instance
+        """
+
+    def unload_world(self, world_name):
+        """
+        Unloads a loaded world by world_name
+        """
+
+        pass
---
+++ redstone/world/World.py
@@ -0,0 +1,56 @@
+from twisted.internet.task import LoopingCall
+
+class World(object):
+
+    def __init__(self):
+        super(World, self).__init__()
+
+        self.world_name = ''
+        self.world_time_max = 24000
+        self.world_time_delay = 0.05
+        self.world_time = 0
+        self.world_age = 0
+
+    def set_world_name(self, world_name):
+        self.world_name = world_name
+
+    def get_world_name(self):
+        return self.world_name
+
+    def set_world_time(self, world_time):
+        self.world_time = world_time
+
+    def get_world_time(self):
+        return self.world_time
+
+    def set_world_age(self, world_age):
+        self.world_age = world_age
+
+    def get_world_age(self):
+        return self.world_age
+
+    def startup(self):
+        # begin world time as ticks
+        self.start_world_time()
+
+    def shutdown(self):
+        # stop world time.
+        self.stop_world_time()
+
+    def start_world_time(self):
+        self.time_loop = LoopingCall(self.tick)
+        self.time_loop.start(self.world_time_delay)
+
+    def tick(self):
+        if self.world_time >= self.world_time_max:
+            # add a world day
+            self.set_world_age(self.get_world_age() + 1)
+
+            # reset the current tick time
+            self.set_world_time(0)
+
+        # update world time
+        self.set_world_time(self.get_world_time() + 1)
+
+    def stop_world_time(self):
+        self.time_loop.stop()
